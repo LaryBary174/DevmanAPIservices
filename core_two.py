2f55748480528842b9a9e09383819cf2312bfe4d
LaryBary174 <lotuc74@gmail.com>
2024-10-24 21:14:59 +0500

    клики по ссылкам с замечаниями

--- core_two.py
+++ core_two.py
@@ -1,47 +1,48 @@
 import requests
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 
 def shorten_link(token: str, url: str):
-    urlAPI = 'https://api.vk.ru/method/utils.getShortLink'
+    url_api = 'https://api.vk.ru/method/utils.getShortLink'
     params = {'access_token': token, 'v': '5.199',
               'p1': 'v1',
               'url': url,
               'private': '0', }
 
-    response = requests.post(url=urlAPI, params=params)
+    response = requests.post(url=url_api, params=params)
     response.raise_for_status()
-    response_json = response.json()
-    result = response_json['response']['short_url']
-    return result
+    response_format = response.json()
+    result_of_response = response_format['response']['short_url']
+    return result_of_response
 
 
 def count_clicks(token: str, short_url_key: str):
-    urlAPI = 'https://api.vk.ru/method/utils.getLinkStats'
+    url_api = 'https://api.vk.ru/method/utils.getLinkStats'
     params = {'access_token': token, 'v': '5.199',
               'p1': 'v1',
               'key': short_url_key,
               'interval': 'forever',
               'extended': '0', }
-    response = requests.post(url=urlAPI, params=params)
+    response = requests.post(url=url_api, params=params)
     response.raise_for_status()
-    result = response.json()
-    return result['response']['stats']
+    result_of_response = response.json()
+    return result_of_response['response']['stats']
 
 
 def is_shorten_link(url):
-    from urllib.parse import urlparse
-    parse_url = urlparse(url)
-    if parse_url.netloc == 'vk.cc' and parse_url.path != '':
-        short_url_key = parse_url.path.lstrip('/')
+    parser_url = urlparse(url)
+    if parser_url.netloc == 'vk.cc' and parser_url.path != '':
+        short_url_key = parser_url.path.lstrip('/')
         return short_url_key
     return False
 
 
 def main():
     load_dotenv()
-    token = os.getenv('TOKEN')
+    token = os.environ['VK_TOKEN_REQUEST']
+
     url = input('Enter URL: ')
     short_url_key = is_shorten_link(url)
     try:
@@ -51,8 +52,13 @@ def main():
         else:
             short_link = shorten_link(token, url)
             print('Сокращенная ссылка: ', short_link)
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         print(f'Ошибка в переданном URL {e}')
+    except ValueError as e:
+        print(f'Получен неверный тип данных {e}')
+    except KeyError as e:
+        print(f'Получен неверный ответ URL {e}')
+
 
 
 if __name__ == '__main__':
